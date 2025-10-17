1dbd2ca2250dd593e6fcba21b0fc0290d00fd9f1
lejs <lsalkic@student.42heilbronn.de>
2025-10-17 18:38:48 +0200

    :(

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: lsalkic <lsalkic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 15:14:12 by lsalkic           #+#    #+#             */
-/*   Updated: 2025/10/17 17:38:20 by lsalkic          ###   ########.fr       */
+/*   Updated: 2025/10/17 18:37:58 by lsalkic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	i = 0;
 	count = 0;
-	while (is_in_set(s1[i++], set))
+	if (!s1 || !set)
+		return (NULL);
+	while (is_in_set(s1[i++], set) && s1[i])
 		count++;
 	copy_i = i;
 	while (s1[i])
@@ -43,13 +45,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (is_in_set(s1[i--], set))
 		count++;
 	new_s = malloc(ft_strlen(s1) - count + 1);
+	if (!new_s)
+		return (NULL);
 	i = 0;
 	while (s1[copy_i] < ft_strlen(s1))
-	{
-		new_s[i] = s1[copy_i];
-		i++;
-		copy_i++;
-	}
+		new_s[i++] = s1[copy_i++];
 	new_s[i] = '\0';
 	return (new_s);
 }
