b7f64d2f40b6fce557f190eea3dc4f47638eb23c
lejs <lsalkic@student.42heilbronn.de>
2025-10-19 10:58:04 +0200

    changessss

--- ft_split.c
+++ ft_split.c
@@ -6,30 +6,77 @@
 /*   By: lsalkic <lsalkic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 13:41:38 by lsalkic           #+#    #+#             */
-/*   Updated: 2025/10/18 19:00:32 by lsalkic          ###   ########.fr       */
+/*   Updated: 2025/10/19 10:57:40 by lsalkic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+size_t	nbr_words(char const *s, char c)
 {
-	int	i;
+	size_t	i;
+	size_t	word_count;
 
 	i = 0;
-	if (s == 0)
-		return (NULL);
-}
-
-char	*in_a_word(char const *s, char c)
-{
-	int	i;
-
-	i = 0;
+	word_count = 0;
 	while (s[i])
 	{
-		while (s[i] == c)
+		while (s[i] && s[i] == c)
 			i++;
+		while (s[i] && s[i] != c)
+		{
+			i++;
+		}
+		if (i > 0 && s[i - 1] != c)
+		{
+			word_count++;
+		}
 	}
-	return (&s[i]);
+	return (word_count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	word;
+	char	**words;
+
+	if (s == NULL)
+		return (NULL);
+	word = nbr_words(s, c);
+	words = malloc(word + 1 * sizeof *words);
+	if (!words)
+		return (NULL);
+	words[word] = NULL;
+}
+
+// count the words
+// allocate memory for array of string
+// copy words in correct position
+
+int	fill(char **words, char const *s, char c)
+{
+	int i;
+	int length;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		length = 0;
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
+		{
+			i++;
+			length++;
+		}
+		if (length > 0)
+		{
+			if (new_malloc(words, i, len + 1))
+			{
+				return (1);
+			}
+		}
+	}
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,73 +6,26 @@
 /*   By: lsalkic <lsalkic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 15:14:12 by lsalkic           #+#    #+#             */
-/*   Updated: 2025/10/18 19:51:28 by lsalkic          ###   ########.fr       */
+/*   Updated: 2025/10/18 21:54:30 by lsalkic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	is_in_set(char c, char const *set)
-{
-	int	i;
-
-	i = 0;
-	while (set[i])
-	{
-		if (set[i] == c)
-			return (true);
-		i++;
-	}
-	return (false);
-}
-
-int	cut_away(char const *s1, char const *set)
-{
-	int	i;
-	int	count;
-	int	copy_i;
-
-	count = 0;
-	i = 0;
-	while (s1[i] && is_in_set(s1[i++], set))
-	{
-		count++;
-		i++;
-	}
-	copy_i = i;
-	while (s1[i])
-		i++;
-	i--;
-	while (i >= copy_i && is_in_set(s1[i], set))
-	{
-		count++;
-		i--;
-	}
-	return (count);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		count;
-	char	*new_s;
-	int		copy_i;
+	int	i;
+	int	len_s1;
 
 	i = 0;
-	count = 0;
-	if (!s1 || !set)
-		return (NULL);
-	count = cut_away(s1, set);
-	new_s = malloc(ft_strlen(s1) - count + 1);
-	if (!new_s)
-		return (NULL);
-	i = 0;
-	while (copy_i < (int)ft_strlen(s1) - (count - (copy_i)))
-	{
-		new_s[i] = s1[copy_i];
-		i++;
-		copy_i++;
-	}
-	new_s[i] = '\0';
-	return (new_s);
+	len_s1 = ft_strlen(s1);
+}
+
+int	in_start(char const *s1, char const *set)
+{
+	int i;
+	int j;
+
+	int i = 0;
+	int j = 0;
 }
