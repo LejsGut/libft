6feff90c4e544d7e0e7f13f37b8d62258248e579
lejs <lsalkic@student.42heilbronn.de>
2025-10-20 19:37:58 +0200

    would love to test but paco does not work..

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,24 +6,17 @@
 /*   By: lsalkic <lsalkic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 15:14:12 by lsalkic           #+#    #+#             */
-/*   Updated: 2025/10/20 18:52:56 by lsalkic          ###   ########.fr       */
+/*   Updated: 2025/10/20 19:37:05 by lsalkic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strtrim(char const *s1, char const *set)
+size_t	ft_start(char const *s1, char const *set)
 {
-	int	i;
-
-	i = 0;
-}
-
-int	ft_start(char const *s1, char const *set)
-{
-	int	i;
-	int	j;
-	int	amount;
+	size_t	i;
+	size_t	j;
+	size_t	amount;
 
 	j = 0;
 	amount = 0;
@@ -38,25 +31,25 @@ int	ft_start(char const *s1, char const *set)
 			return (amount);
 		j++;
 	}
-	return (0);
+	return (amount);
 }
 
-int	ft_end(char const *s1, char const *set)
+size_t	ft_end(char const *s1, char const *set)
 {
-	int	i;
-	int	length;
-	int	bmount;
+	size_t	i;
+	size_t	length;
+	size_t	bmount;
 
 	bmount = 0;
-	length = ft_strlen(s1) - 1;
-	if (length < 0)
+	length = ft_strlen(s1);
+	if (length == 0)
 		return (0);
-	while (length >= 0)
+	while (length > 0)
 	{
 		i = 0;
-		while (set[i] && set[i] != s1[length])
+		while (set[i] && set[i] != s1[length - 1])
 			i++;
-		if (set[i] == s1[length])
+		if (set[i] == s1[length - 1])
 		{
 			bmount++;
 			length--;
@@ -74,11 +67,13 @@ char	*all_together(char const *s1, char const *set)
 	size_t	new_len;
 	char	*trimmed;
 
+	if (!s1 || !set)
+		return (NULL);
 	all = ft_strlen(s1);
 	start = ft_start(s1, set);
 	end = ft_end(s1, set);
-	if ((start + end) > all)
-		return (new_len = 0);
+	if (start > all - end)
+		new_len = 0;
 	else
 		new_len = all - (start + end);
 	trimmed = malloc(new_len + 1);
@@ -86,5 +81,45 @@ char	*all_together(char const *s1, char const *set)
 		return (NULL);
 	else
 		trimmed[new_len] = '\0';
+	return (trimmed);
+}
+size_t	len(char const *s1, char const *set)
+{
+	size_t	all;
+	size_t	start;
+	size_t	end;
+	size_t	new_len;
+
+	all = ft_strlen(s1);
+	start = ft_start(s1, set);
+	end = ft_end(s1, set);
+	if (start > all - end)
+		new_len = 0;
+	else
+		new_len = all - (start + end);
+	return (new_len);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trimmed;
+	size_t	as_of;
+	size_t	i;
+	size_t	new_len;
+
+	i = 0;
+	if (!s1 || !set)
+		return (NULL);
+	trimmed = all_together(s1, set);
+	if (!trimmed)
+		return (NULL);
+	as_of = ft_start(s1, set);
+	new_len = len(s1, set);
+	while (i < new_len)
+	{
+		trimmed[i] = s1[as_of + i];
+		i++;
+	}
+	trimmed[new_len] = '\0';
 	return (trimmed);
 }
